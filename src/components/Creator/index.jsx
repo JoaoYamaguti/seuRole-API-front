79dc601c6f4c521da781f50120b72d4dcfcade65
João Yamaguti <joaov.yamaguti@gmail.com>
2023-10-19 21:58:02 -0300

    remove create backup

--- src/components/Creator/index.jsx
+++ src/components/Creator/index.jsx
@@ -7,7 +7,7 @@ import { questions } from "../../data/questions.jsx";
 import { daysOfWeek } from "../../data/days.jsx";
 
 import "./style.css";
-import { backup } from "../../data/backup";
+// import { backup } from "../../data/backup";
 
 export default function Creator({ handleChange }) {
   let asks = questions;
@@ -131,10 +131,10 @@ export default function Creator({ handleChange }) {
     setCategoriesValue({ id: index, option: option.value })
 
   }
-  async function createBackup() {
-    backup.forEach(async (estab) => await API.post("establishment/create", estab))
-    ;
-  }
+
+  // async function createBackup() {
+  //   backup.forEach(async (estab) => await API.post("establishment/create", estab))
+  // }
 
   useEffect(() => {
     timeHours.open !== ""
@@ -161,7 +161,8 @@ export default function Creator({ handleChange }) {
     <form id="cadastro" onSubmit={(event) => event.preventDefault()}>
       <fieldset>
         <legend>Cadastro</legend>
-        <p>Preencha os campos para cadastrar um estabelecimento.</p> <button onClick={() => createBackup()}>createBackup</button>
+        <p>Preencha os campos para cadastrar um estabelecimento.</p> 
+        {/* <button onClick={() => createBackup()}>createBackup</button> */}
         <span>
           <label htmlFor="name">Nome:</label>
           <input
